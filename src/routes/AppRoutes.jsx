f6ede8786df9cd0d6399a97f0a230c9f41a32bc8
Koli Rani Das <kolidash02@gmail.com>
2026-08-16 03:16:55 +0600

    Add department management UI

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -2,19 +2,23 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import Login from "../pages/auth/Login";
 import AdminDashboard from "../pages/admin/Dashboard";
 import Students from "../pages/admin/Students";
+import Departments from "../pages/admin/Departments";
 import TeacherDashboard from "../pages/teacher/Dashboard";
 import StudentDashboard from "../pages/student/Dashboard";
-import Departments from "../pages/admin/Departments";
 
 function AppRoutes() {
   return (
     <Routes>
-      <Route path="/admin/departments" element={<Departments />} />
-      <Route path="/student/dashboard" element={<StudentDashboard />} />
-      <Route path="/teacher/dashboard" element={<TeacherDashboard />} />
       <Route path="/" element={<Login />} />
+
       <Route path="/admin/dashboard" element={<AdminDashboard />} />
       <Route path="/admin/students" element={<Students />} />
+      <Route path="/admin/departments" element={<Departments />} />
+
+      <Route path="/teacher/dashboard" element={<TeacherDashboard />} />
+
+      <Route path="/student/dashboard" element={<StudentDashboard />} />
+
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );

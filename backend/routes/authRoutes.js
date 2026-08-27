16816aa01c0fe8f6069e9e582705868e70b91e1c
Punom <punomkripanwita@gmail.com>
2026-08-27 21:26:20 +0600

    Add role based authorization

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const { registerUser, loginUser, getMe } = require("../controllers/authController");
 const protect = require("../middleware/authMiddleware");
+const authorizeRoles = require("../middleware/roleMiddleware");
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.get("/me", protect, getMe);
+
+router.get("/admin-test", protect, authorizeRoles("admin"), (req, res) => {
+  res.json({
+    message: "Welcome Admin! You have admin access.",
+  });
+});
 
 module.exports = router;
